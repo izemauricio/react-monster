5c95fca9905f29ddb0884f5f41b5d2db4e727bc0
Mauricio Ize <mauriciodcjz@gmail.com>
2020-03-27 14:45:57 -0300

    Updates

---
+++ precache-manifest.ddda5aa313d0191d77dce382ff699985.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4741bd743dcb1b8cfd360005bb8d3ef4",
+    "url": "/react-web-monster/index.html"
+  },
+  {
+    "revision": "3c46c1a085c37b880af0",
+    "url": "/react-web-monster/static/css/main.335cfb9d.chunk.css"
+  },
+  {
+    "revision": "3812844254aa3357195a",
+    "url": "/react-web-monster/static/js/2.97ca5a54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-web-monster/static/js/2.97ca5a54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c46c1a085c37b880af0",
+    "url": "/react-web-monster/static/js/main.5f68202a.chunk.js"
+  },
+  {
+    "revision": "9675e7480a280e6e852f",
+    "url": "/react-web-monster/static/js/runtime-main.5ea899f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-web-monster/static/media/logo.5d5d9eef.svg"
+  }
+]);
